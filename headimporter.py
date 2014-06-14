9e7ba895436f97edbe71479d568f4ae0a6bd1e3b
Haml3t <lhaml3t@gmail.com>
2014-06-14 14:30:09 -0700

    no more kludgedict are you happy

--- headimporter.py
+++ headimporter.py
@@ -1,10 +1,21 @@
 import csv
+import re
+import json
+
 mainHead = {}
-def kludgeDict(values,header):
+def makeHeadPieceDict(values,header):
 	headPiece = {}
 	for i in range(len(header)):
-		headPiece[header[i]] = values[i]
+		if values[i]:
+			
+			if i == 0:
+				plusIndex = values[0].find('+')
+				name = values[0][:plusIndex]
+				headPiece[header[i]] = name
+			else:
+				headPiece[header[i]] = values[i]
 	return headPiece
+
 with open('Dark Souls 2 Armor - Head.csv', 'r') as armorCsv:
 	armorReader = csv.reader(armorCsv, dialect='excel')
 	isFirstRow = True
@@ -12,6 +23,25 @@ with open('Dark Souls 2 Armor - Head.csv', 'r') as armorCsv:
 		if isFirstRow:
 			header = row
 			isFirstRow = False
-		else:
-			mainHead[row[0]] = kludgeDict(row,header)
-print mainHead['Vengarl\'s Helm']
+		elif re.match('.+\+\d+.*',row[0]):
+			"""
+			if row[0] in mainHead:
+				if row[0] == 'Dingy Hood':
+					if row[0] == 21:
+						mainHead[row[0]] = makeHeadPieceDict(row,header)
+					else
+					#skip to next row
+				else
+				mainHead[row[0]] = makeHeadPieceDict(row,header)
+			else
+			"""
+			plusIndex = row[0].find('+')
+			name = row[0][:plusIndex]
+			mainHead[name] = makeHeadPieceDict(row,header)
+
+print mainHead['Old Ironclad Helm']
+
+
+with open('headarmor.json', 'w') as fp:
+	json.dump(mainHead,fp)
+
