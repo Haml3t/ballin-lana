cfb85d959478a81113ea0e2ff77a8c6add1af0a6
Haml3t <lhaml3t@gmail.com>
2014-06-20 09:57:56 -0700

    stamina calculator

--- performancecalc.py
+++ performancecalc.py
@@ -1,5 +1,7 @@
 from attributes import VGR, END, VIT, ATN, STR, DEX, ADP, INT, FTH
 
+
+
 def equipLoad(VIT):
 	baseEquip = 38.5
 	equip = baseEquip + equipBonus(VIT)
@@ -13,9 +15,50 @@ def equipBonus(VIT):
 	elif VIT >= 50 and VIT <= 69:
 		bonus = equipBonus(49) + 0.5*(VIT-49)
 	else:
-		bonus - equipBonus(69) + .5*(2*(.25*(VIT-69))-2)
+		bonus = equipBonus(69) + .5*(2*(.25*(VIT-69))-2)
 	return bonus
 
-print equipLoad(VIT)
+print 'Equip Load = ' + str(equipLoad(VIT))
 
-def 
+def HP(VGR, END, VIT, ATN, STR, DEX, ADP, INT, FTH):
+	baseHP = 500
+	HP = baseHP + VGRHPGain(VGR) + lowHPGain(END) + lowHPGain(VIT) + lowHPGain(ATN) + lowHPGain(STR) + lowHPGain(DEX) + lowHPGain(ADP) + lowHPGain(INT) + lowHPGain(FTH)
+	return HP
+
+def VGRHPGain(VGR):
+	if VGR >= 0 and VGR <= 20:
+		bonus = 30*VGR
+	elif VGR >= 21 and VGR <= 50:
+		bonus = VGRHPGain(20) + 20*(VGR-20)
+	else:
+		bonus = VGRHPGain(50) + 5*(VGR-50)
+	return bonus
+	
+def lowHPGain(attribute):
+	if attribute >= 0 and attribute <= 20:
+		bonus = 2*attribute
+	elif attribute >= 21 and attribute <= 50:
+		bonus = lowHPGain(20) + (attribute-20)
+	else:
+		bonus = lowHPGain(50)
+	return bonus
+	
+print 'HP = ' + str(HP(VGR, END, VIT, ATN, STR, DEX, ADP, INT, FTH))
+
+def stamina(END):
+	baseStamina = 80
+	stamina = baseStamina + staminaBonus(END)
+	return stamina
+
+def staminaBonus(END):
+	if END >= 0 and END <= 20:
+		bonus = 2*END
+		return bonus
+	else:
+		bonus = staminaBonus(20) + (END - 20)
+		return bonus
+
+print 'Stamina = ' + str(stamina(END))
+
+def magicBNS(INT):
+	
